a6e9876a4f0d02143d89cf53191c001c0488b99d
alunturner <alun.turner@googlemail.com>
2023-10-15 14:38:08 +0100

    add breadcrumb handling

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,38 +9,62 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
-const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={32} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
+const ShoeBreadcrumbs = () => {
+    return <Breadcrumbs>
+        <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+        <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+        <Breadcrumbs.Crumb href="/sale/shoes">
             Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+        </Breadcrumbs.Crumb>
+    </Breadcrumbs>
+}
+
+const ShoeIndex = ({ sortId, setSortId }) => {
+    return (
+        <Wrapper>
+            <MainColumn>
+                <Header>
+                    <OtherBreadcrumbs>
+                        <ShoeBreadcrumbs />
+                    </OtherBreadcrumbs>
+                    <Title>Running</Title>
+                    <Select
+                        label="Sort"
+                        value={sortId}
+                        onChange={(ev) => setSortId(ev.target.value)}
+                    >
+                        <option value="newest">Newest Releases</option>
+                        <option value="price">Price</option>
+                    </Select>
+                </Header>
+                <Spacer size={32} />
+                <ShoeGrid />
+            </MainColumn>
+            <LeftColumn>
+                <DesktopBreadcrumbs>
+                    <ShoeBreadcrumbs />
+                </DesktopBreadcrumbs>
+                <Spacer size={42} />
+                <ShoeSidebar />
+            </LeftColumn>
+        </Wrapper>
+    );
 };
+
+const DesktopBreadcrumbs = styled.div`
+    @media ${QUERIES.tabletAndSmaller} {
+        display: none;
+    }
+`
+const OtherBreadcrumbs = styled.div`
+    display: none;
+    @media ${QUERIES.tabletAndSmaller} {
+        display: revert;
+        position: absolute;
+        top: -12px;
+        left: 0;
+    }
+`
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,6 +75,10 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+      display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +89,7 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+    position: relative;
 `;
 
 const Title = styled.h2`
